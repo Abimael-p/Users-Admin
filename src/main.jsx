06c1aab675edfb596ab2a3fead60fc7f139b2db1
Abimael pacheco <pachecoabimael360@gmail.com>
2023-07-06 22:38:15 -0500

    nbfi

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter, RouterProvider } from "react-router-dom";
-import { router } from "./router";
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { RouterProvider } from 'react-router-dom';
+import { router } from './router';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <RouterProvider router={router} />
-    </BrowserRouter>
+        <RouterProvider router={router} />
   </React.StrictMode>
 );
+
